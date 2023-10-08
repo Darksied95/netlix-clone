b4889adc5e2d20a4737412f27acf12431ac8288c
Darksied95 <aliyuefe9501@gmail.com>
2023-10-08 10:55:05 +0100

    add globalStyle and normalizeCss

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'normalize.css'
+import { GlobalStyles } from "./global.styles.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <>
+        <GlobalStyles />
+        <App />
+    </>
 )
