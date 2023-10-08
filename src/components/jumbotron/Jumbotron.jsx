e839b775b3e448fef25c71ead521fb3e0800704c
Darksied95 <aliyuefe9501@gmail.com>
2023-10-08 10:36:43 +0100

    restructure jumbotron

--- src/components/jumbotron/Jumbotron.jsx
+++ src/components/jumbotron/Jumbotron.jsx
@@ -1,4 +1,4 @@
-import { Inner, Container } from "./styles/jumbotron.styles"
+import { Inner, Container, Pane, Title, SubTitle, Image } from "./styles/jumbotron.styles"
 import PropTypes from 'prop-types'
 
 const Jumbotron = ({ children, direction = 'row', ...restProps }) => {
@@ -12,12 +12,40 @@ const Jumbotron = ({ children, direction = 'row', ...restProps }) => {
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>
 }
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>
+}
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>
+}
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+Jumbotron.Image = function JumbotronSubTitle({ ...restProps }) {
+    return <Image {...restProps} />
+}
 
 export default Jumbotron
 
 
 Jumbotron.propTypes = {
     direction: PropTypes.string,
+    children: PropTypes.node.isRequired,
+    restProps: PropTypes.array
+}
+Jumbotron.Pane.propTypes = {
+    children: PropTypes.node.isRequired,
+    restProps: PropTypes.array
+}
+Jumbotron.Image.propTypes = {
+    children: PropTypes.node.isRequired,
+    restProps: PropTypes.array
+}
+Jumbotron.Title.propTypes = {
+    children: PropTypes.node.isRequired,
+    restProps: PropTypes.array
+}
+Jumbotron.SubTitle.propTypes = {
     children: PropTypes.node.isRequired,
     restProps: PropTypes.array
 }
