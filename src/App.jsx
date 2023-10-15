b3cb2addb97cd6aae3e5f2886c7fb94e8e33aa3a
Darksied95 <aliyuefe9501@gmail.com>
2023-10-15 20:35:48 +0100

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { FooterContainer } from "./containers/footer"
 import JumbotronContainer from "./containers/jumbotron"
 
 
 function App() {
 
   return (
-    <JumbotronContainer />
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   )
 }
 
